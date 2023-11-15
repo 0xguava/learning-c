295272976423cb90eb7d14211061c34482f3e501
0xguava <demo.lappy@protonmail.com>
2023-11-15 08:06:24 +0530

    lol

--- matrix/add-matrix.c
+++ matrix/add-matrix.c
@@ -5,7 +5,7 @@ int print_matrix(int r,int c,int matrix[r][c]);
 
 int main(){
   int matA[3][3]={{1,2,-1},{3,2,0},{-4,0,2}}, matB[3][3]={{3,4,2},{0,1,0},{-2,0,1}}, matC[3][3];
-  int i, j, k=0, m, n,sum=0;
+  int i, j, k=0, m=0, n=0,sum=0;
 
 
   /* printf("Enter Matrix A:\n"); */
@@ -24,9 +24,15 @@ int main(){
     for(j=0;j<3;j++){
       matC[k][i] = matC[k][i] + (matA[i][j] * matB[j][i]); 
     }
-  }
-   
     k += 1;  
+  }
+  
+  for(i=0;i<2;i++){
+    for(j=0;j<3;j++){
+      matC[i+1][i] =  matC[i+1][i] + (matA[i+1][j] * matB[j][i]);
+    }
+  } 
+   
   print_matrix(3,3,matA);
   printf("\n");
   print_matrix(3,3,matB);
